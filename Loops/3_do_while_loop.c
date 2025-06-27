6bc3877addae4a730d3dfaddcd610d21c70ce182
Shahan Samar <shahansamar16@gmail.com>
2025-06-27 13:05:41 +0530

    Create 3_do_while_loop.c
    MCQ based question about Do-While loop.

---
+++ Loops/3_do_while_loop.c
@@ -0,0 +1,8 @@
+#include<stdio.h> //Problem 3: A "DO-WHILE LOOP" is executed:
+                  // a. At least ones
+int main(){       // b. At least twice
+                  // c. At most once
+                  // answer- at least ones. (loop cuz more than 1 time)
+
+    return 0;
+}
