801f0daa2ab7b61f62c0706dd2a82eaa1474f7d3
Shahan Samar <shahansamar16@gmail.com>
2025-06-27 12:45:13 +0530

    Create 5_invalid_check.c
    MCQ based question, uses previous understanding of to get this answer correct instead of writing a code.

---
+++ VariablesAndDatatype/5_invalid_check.c
@@ -0,0 +1,10 @@
+#include<stdio.h> // Problem 1: which one of the following is invalid in C?
+                    // a> int a = 1; int b = a;
+                    // b> int v = 3*3
+                    // c> char dt = '21 dec 2020':
+int main(){         // asnwer : c>
+ 
+
+
+    return 0;
+}
