5e590e2ca71a134b7719341e324dd96ae954804f
Shahan Samar <shahansamar16@gmail.com>
2025-06-27 13:06:16 +0530

    Create 4_MCQ.c
    MCQ based problem.

---
+++ Loops/4_MCQ.c
@@ -0,0 +1,9 @@
+#include<stdio.h> //Problem 4: What can be done using one type of loop can also be done using the other two 
+                  // type of loops. True or False
+                  // True
+int main(){
+
+
+
+    return 0;
+}
